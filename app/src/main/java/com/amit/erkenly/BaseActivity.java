0b84508ca2d1d858271d2e8b69790f8627f0d3bf
AmrZizo <aabdelaziz@panarab-media.com>
2020-05-16 18:34:14 +0200

    Initial Commit

--- app/src/main/java/com/amit/erkenly/BaseActivity.java
+++ app/src/main/java/com/amit/erkenly/BaseActivity.java
@@ -10,5 +10,6 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
     }
 }
